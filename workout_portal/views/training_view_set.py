086feb09657f625618828fa708c89d955423e5f4
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-12-03 09:48:28 +0300

    - unused method removed

--- workout_portal/views/training_view_set.py
+++ workout_portal/views/training_view_set.py
@@ -7,6 +7,3 @@ from workout_portal.service.training_service import training_service
 class TrainingViewSet(viewsets.ModelViewSet):
     queryset = training_service.list()
     serializer_class = TrainingSerializer
-
-    # def retrieve(self, request, pk=None):
-    #     return training_service.get_by_id(training_id=pk)
