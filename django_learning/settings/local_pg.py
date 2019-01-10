39b07bcfe5fb76d32752b235f7c0a855b4328f19
aiharisov <aiharisov@gmail.com>
2019-01-11 00:18:26 +0300

    temp

--- django_learning/settings/local_pg.py
+++ django_learning/settings/local_pg.py
@@ -5,9 +5,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'workout_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'NAME': 'django_learning',
+        'USER': 'django_learning',
+        'PASSWORD': 'django_learning',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
