d4fb5eab8a63e201abce37d96491fe586417015e
Nikita <Nikita.Titov@waveaccess.ru>
2019-01-16 17:27:45 +0300

    Change connect of bootstrap in project

--- django_learning/settings/base.py
+++ django_learning/settings/base.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'workout_portal.apps.WorkoutPortalConfig',
+    'workout_portal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
