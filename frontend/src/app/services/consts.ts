a6f7bd567e8e17d7e33efb78e11d1bf9da82ee66
Ilya Churakov <Ilya.Churakov@waveaccess.ru>
2019-01-21 15:59:21 +0300

    Changed rest api port

--- frontend/src/app/services/consts.ts
+++ frontend/src/app/services/consts.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000/api/';
+export const API_BASE_URL = 'http://localhost:8080/api/';
 /**Trainings */
 const TRAININGS_BASE = `${API_BASE_URL}trainings/`;
 export const GET_TRAININGS = `${TRAININGS_BASE}`;
